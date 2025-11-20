26f3bf17649f14dfd036f29d07a916a7dd79d81a
Seth M. de Almeida Sobrinho <seth.sobrinho@amcom.com.br>
2025-11-19 23:44:14 -0300

    initialcomit

--- tests/ContaCorrente.Tests/obj/Release/net9.0/ContaCorrente.Tests.AssemblyInfo.cs
+++ tests/ContaCorrente.Tests/obj/Release/net9.0/ContaCorrente.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContaCorrente.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf9e7dab09385d06f84be5802ec4a566d9f6ae84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6be760507071a80c0ef679a34ae9f9341d1975cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContaCorrente.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContaCorrente.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
